4610c680ad006fc0b0e265b8f4f89ba02b554633
sohyun <sh0000@pusan.ac.kr>
2024-10-16 16:36:29 +0900

    feat: create member repository and test

---
+++ src/main/java/com/server/springboot/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package com.server.springboot.repository;
+
+import com.server.springboot.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); // 값을 먼저 증가시키고 적용됨
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // Optional로 감싸주면 null이어도 예외처리 안됨
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ src/test/java/com/server/springboot/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.server.springboot.repository;
+
+import com.server.springboot.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class MemoryMemberRepositoryTest {
+    MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @AfterEach // 각 테스트 후에 정리 작업을 수행한다. cf. BeforeEach는 테스트 전에 수행
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        memberRepository.save(member);
+        Member result = memberRepository.findById(member.getId()).get();
+        Assertions.assertEquals(result, member);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberRepository.save(member2);
+
+        Member result = memberRepository.findByName("spring1").get();
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
