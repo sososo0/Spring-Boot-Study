510a7213dd6ed212ba1ffc2eed71941904ad2075
sohyun <sh0000@pusan.ac.kr>
2024-10-16 18:12:55 +0900

    feat: create member listing function

--- src/main/java/com/server/springboot/controller/MemberController.java
+++ src/main/java/com/server/springboot/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.server.springboot.domain.Member;
 import com.server.springboot.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,5 +34,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
