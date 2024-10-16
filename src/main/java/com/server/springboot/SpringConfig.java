d5c5a632bf79002e1ac6ea59dd6ec03fbb1ffa04
sohyun <sh0000@pusan.ac.kr>
2024-10-16 21:46:13 +0900

    feat: handling spring data using spring data jpa

--- src/main/java/com/server/springboot/SpringConfig.java
+++ src/main/java/com/server/springboot/SpringConfig.java
@@ -2,7 +2,6 @@ package com.server.springboot;
 
 import com.server.springboot.repository.*;
 import com.server.springboot.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,23 +11,24 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final EntityManager entityManager;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JDBCMemberRepository(dataSource);
 //        return new JDBCTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(entityManager);
-    }
+//        return new JpaMemberRepository(entityManager);
+
+//    }
 }
